e30314b7a4bb0eee6813a16bdcc5139f386b982e
Pachara004 <147172411+Pachara004@users.noreply.github.com>
2024-08-10 17:47:02 +0700

    sd

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const server = http.vreateServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello World!!");
 });
